c311224565037c91059012085a22e292be385e3c
Anak2 <ilovekdh1208@ajou.ac.kr>
2024-02-18 20:04:14 +0900

    test 실행

--- app/src/main/java/githubaction/practice/App.java
+++ app/src/main/java/githubaction/practice/App.java
@@ -13,6 +13,6 @@ public class App {
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        System.out.println(new App().testMethod());
     }
 }
